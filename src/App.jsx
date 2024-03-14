d141314fbf6c30ae4685de6c925ad7e1996517e7
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-03-14 13:25:51 +0530

    adding style side bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import Account from "./pages/Account";
 import Settings from "./pages/Settings";
 import Users from "./pages/Users";
+import Cabins from "./pages/Cabins";
 import Bookings from "./pages/Bookings";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./pages/AppLayout";
@@ -19,6 +20,7 @@ function App() {
             <Route path="dashboard" element={<Dashboard />}></Route>
             <Route path="bookings" element={<Bookings />}></Route>
             <Route path="users" element={<Users />}></Route>
+            <Route path="cabins" element={<Cabins />}></Route>
             <Route path="settings" element={<Settings />}></Route>
             <Route path="account" element={<Account />}></Route>
           </Route>
