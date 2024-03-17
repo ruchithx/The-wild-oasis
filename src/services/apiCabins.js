11dbadbafca1ca0355393bfd6347ec15214b5765
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-03-17 15:50:10 +0530

    changes

---
+++ src/services/apiCabins.js
@@ -0,0 +1,10 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+  if (error) {
+    console.error("Error getting cabins", error);
+    throw new Error("Error getting cabins");
+  }
+  return data;
+}
---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://parestbrefznnzxaeutf.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBhcmVzdGJyZWZ6bm56eGFldXRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0MDE0MzEsImV4cCI6MjAyNTk3NzQzMX0.aavQr91kgI5bOvHXwtoSDvX5PikQNBrgPWLEUT_UquQ";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
