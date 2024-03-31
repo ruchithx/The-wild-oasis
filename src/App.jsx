19890fc034abcb2b71147bcf043242c9b46ba2bb
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-03-31 18:52:37 +0530

    initial react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import Dashboard from "./pages/Dashboard";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
@@ -9,9 +12,19 @@ import Cabins from "./pages/Cabins";
 import Bookings from "./pages/Bookings";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./pages/AppLayout";
+
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60, // 5 minutes
+      },
+    },
+  });
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -28,7 +41,7 @@ function App() {
           <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
