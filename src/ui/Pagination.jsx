eeb03ff99499d6d41f747bc57a074cb22f86a370
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-04-24 08:29:03 +0530

    add pagination part

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -80,11 +80,16 @@ function Pagination({ count }) {
     }
   }
 
+  if (pageCount <= 1) return null;
+
   return (
     <StyledPagination>
       <P>
         Showing <span>{(currentPage - 1) * PAGE_SIZE + 1}</span> to
-        <span>{currentPage * PAGE_SIZE}</span> of <span>{count}</span> results
+        <span>
+          {currentPage === pageCount ? count : currentPage * PAGE_SIZE}
+        </span>{" "}
+        of <span>{count}</span> results
       </P>
       <Buttons>
         <PaginationButton onClick={prevPage} disabled={currentPage === 1}>
